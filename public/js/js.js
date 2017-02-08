b9413ea455fe9217c7f376d820ddaea5bcb50282
mizimax <admin@mizimax.com>
2017-02-08 18:25:20 +0700

    Signed-off-by: mizimax <admin@mizimax.com>

--- public/js/js.js
+++ public/js/js.js
@@ -1,7 +1,20 @@
+var mmenu=false;
 $(document).ready(function(){
-	smoothScroll.init({speed:1500});
+	smoothScroll.init({speed:1000});
 	$('#home').parallax("50%",0.4);
-	new WOW().init();
+  new WOW().init();
+  $('.mmenu').click(function(){
+    if(mmenu==false){
+        $('#menu li').css('display','inline-block');
+        $('.mmenu span').attr('class','glyphicon glyphicon-remove');
+        mmenu=true;
+    }
+    else{
+        $('#menu li').attr('style','');
+        $('.mmenu span').attr('class','glyphicon glyphicon-menu-hamburger');
+        mmenu=false;
+    }
+  });
 });
 
 $.fn.scrollEnd = function(callback, timeout) {          
@@ -24,6 +37,6 @@ $(window).scroll(function(){
 });
 
 $(window).scrollEnd(function(){
-    $('.menu').slideDown('slow');
+    $('.menu').slideDown('slow','swing');
     onScroll=false;
-}, 1000);
+}, 800);
--- resources/views/comcamp.blade.php
+++ resources/views/comcamp.blade.php
@@ -6,18 +6,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="./css/css.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <link rel="stylesheet" href="./css/responsive.css">
+  <link rel="stylesheet" href="./css/animate.min.css">
 </head>
 <body>
 <div id="page">  
-  <div id="home" class="section">
-      <div class="parallaxx" alt=""></div>
+  <div id="home" class="section" align="center">
+      <img src="img/logo.png" width="500px" alt="">
   </div>
   <div id="comcamp" class="section">
-
+    <div class="container">
+      <div class="row">
+        <h1 align="center" class="wow bounceInDown" style="margin:50px">Comcamp คืออะไร ?</h1>
+        <div class="col-md-6">
+          <p id="left" class="wow leftTran"  data-wow-delay="0.5s" data-wow-duration="0.7s" style="margin-left: 100%">wow leftTran</p>
+        </div>
+        <div class="col-md-6">
+            <h2>Comcamp 29th</h2>
+    <div class="wow fadeInUp"  data-wow-delay="1.5s" data-wow-duration="0.7s" style="word-wrap: break-word;">
+    <dd>โครงการฝึกอบรมเชิงปฏิบัติการคอมพิวเตอร์เบื้องต้น ครั้งที่29</dd>
+    <dd>น้องๆเคยสงสัยกันบ้างหรือเปล่า ว่าวิศวะคอมทำอะไรกัน? สนุกไหม? แล้วเสน่ห์ของวิศวะคอมคืออะไรกันนะ?งั้นมาเข้าค่าย Comcamp 29th กันเถอะ!</dd> มาลองสัมผัสความเป็นนักศึกษาวิศวะคอมพิวเตอร์ มาเรียนรู้ มาลงมือทำจริง ซึมซับประสบการณ์ใหม่ๆ และมาพับกบ เอ้ย! พบกับเพื่อนๆมากหน้าหลายตา และที่สำคัญค่ายนี้...ฟรีทั้งค่าย พี่ๆกำลังรอน้องๆมาเป็นส่วนหนึ่งของค่ายอยู่นะ รีบสมัครกันเข้ามาเยอะๆน้า
+    </div>
+          
+        </div>
+      </div>
+    </div>
   </div>
   <div id="learning" class="section">
-    ASDSAD
+    
   </div>
   <div id="schedule" class="section">
     
@@ -37,13 +53,17 @@
   </div>
 </div>
 
-<div class="menu container">
-  <a data-scroll href="#home" class="action-button shadow animate blue">Home</a>
-  <a data-scroll href="#comcamp" class="action-button shadow animate red">SADSAD</a>
-  <a data-scroll href="#learning" class="action-button shadow animate green">SADSAD</a>
-  <a data-scroll href="#schedule" class="action-button shadow animate blue">SADSAD</a>
-  <a data-scroll href="#contact" class="action-button shadow animate yellow">Contact Us</a>
-</div>
+<nav id="menu" class="menu container">
+  <ul class="main">
+  <li><a data-scroll href="#home" class="action-button animate"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+  <li><a data-scroll href="#comcamp" class="action-button animate">Comcamp</a></li>
+  <li><a data-scroll href="#learning" class="action-button animate">Learning</a></li>
+  <li><a data-scroll href="#schedule" class="action-button animate">Schedule</a></li>
+  <li><a data-scroll href="#contact" class="action-button animate">Contact</a></li>
+  <br>
+  <a class="mmenu"><span class="glyphicon glyphicon-menu-hamburger" style="font-size: 30px;font-color:#000;padding-top: 9px"></span></a>
+  </ul>
+</nav>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -52,7 +72,6 @@
   <script src="https://npmcdn.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
   <script src="./js/smooth-scroll.min.js"></script>
   <script src="./js/js.js"></script>
-  <script>
-  </script>
+  <script>new WOW().init();</script>
 </body>
 </html>
